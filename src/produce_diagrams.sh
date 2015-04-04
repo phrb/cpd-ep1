8cd5ffc27e48c223cd3bed1337791926afcec2c7
mai <mai@localhost>
2015-04-04 11:18:12 -0300

    first diagrams (produce with ./produce_diagrams.sh)

---
+++ src/produce_diagrams.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+gnuplot showresult_seq_vs_1thread_amd.dem > seq_vs_1thread_amd.png
+gnuplot showresult_seq_vs_1thread_c2d.dem > seq_vs_1thread_c2d.png
+gnuplot showresult_old_new.dem > old_new.png
+
