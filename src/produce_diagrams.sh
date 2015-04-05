0321e625fd027c22563b177347f63f6f45e1aa31
mai <mai@localhost>
2015-04-05 11:45:37 -0300

    small fixes, new measurement data and new diagrams

--- src/produce_diagrams.sh
+++ src/produce_diagrams.sh
@@ -3,4 +3,5 @@
 gnuplot showresult_seq_vs_1thread_amd.dem > seq_vs_1thread_amd.png
 gnuplot showresult_seq_vs_1thread_c2d.dem > seq_vs_1thread_c2d.png
 gnuplot showresult_old_new.dem > old_new.png
+gnuplot showresult_par_threads.dem
 
