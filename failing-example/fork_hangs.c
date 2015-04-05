1cef365a57f3e2fad1cf2dd1fce0f29035685664
phrb <pedro.bruel@gmail.com>
2015-04-05 12:40:03 -0300

    Prettified the code and docs, and added
    more documentation text.

--- failing-example/fork_hangs.c
+++ failing-example/fork_hangs.c
@@ -2,36 +2,29 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void a()
-{
+void a(){
     #pragma omp parallel num_threads(2)
     {
         puts("para_a");
     }
     puts("a ended");
 }
-
-void b()
-{
+void b(){
     #pragma omp parallel num_threads(2)
     {
         puts("para_b");
     }
     puts("b ended");
 }
-
-int main()
-{
+int main(){
     a(); //Inicializa uma thread pool no processo pai.
     int p = fork();
-    if(!p)
-    {
+    if(!p){
         printf("id=%d\n", p);
         b(); // Inicializa uma thread pool no processo filho.
         _exit(0);
     }
-    else
-    {
+    else{
         printf("id=%d\n", p); // Dentro do processo pai.
     }
     wait(NULL); // Espera que b() retorne o controle.
